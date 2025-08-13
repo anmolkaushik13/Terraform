96bd518896d7271aaa3e8524db77f1c1970f37a7
Anmol Kaushik <108117299+anmolkaushik13@users.noreply.github.com>
2025-08-13 17:28:34 +0530

    Add files via upload

---
+++ provide.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.30.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+  subscription_id = "3be29ba2-b31c-4fbd-a7bb-c51bf50ef740"
+}
